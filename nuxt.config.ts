74cc3b4b5729473422192d42480ecad9f622d977
josemusa <musajoseph2017@gmail.com>
2024-03-24 23:28:20 +0300

    pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -66,15 +66,15 @@ export default defineNuxtConfig({
         },
       ],
     },
-    workbox: {
-      navigateFallback: "/",
-    },
     // workbox: {
     //   navigateFallback: "/",
-    //   globDirectory: ".nuxt/dist/client",
-    //   globPatterns: ["**/*.{js,json}", "**/node_modules/**/*"],
-    //   globIgnores: ["**/node_modules/**/*"],
     // },
+    workbox: {
+      navigateFallback: "/",
+      globDirectory: ".nuxt/dist/client",
+      globPatterns: ["**/*.{js,json}", "**/node_modules/**/*"],
+      globIgnores: ["**/node_modules/**/*"],
+    },
     client: {
       installPrompt: true,
     },
