62d30969124952bbd929e3d75878593e8cac39eb
josemusa <musajoseph2017@gmail.com>
2024-06-21 18:19:47 +0300

    app file

--- app.vue
+++ app.vue
@@ -11,7 +11,7 @@ const installPwa = async () => {
 <template>
   <NuxtPwaManifest />
   <div>
-    <div
+    <!-- <div
       class="max-w-screen-sm z-20 absolute bottom-4 bg-white/20 backdrop-blur-md mx-6 p-4 rounded-2xl border-2 border-gray-100"
       v-if="installationPromptIsShown">
       <p class="text-xs font-medium">
@@ -27,7 +27,7 @@ const installPwa = async () => {
           Some other time
         </button>
       </div>
-    </div>
+    </div> -->
     <NuxtPage />
   </div>
 </template>
