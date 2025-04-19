11b44b5846835e887cb8aae95feeabfe77b9da4b
musajose <78262428+musamutuku@users.noreply.github.com>
2025-04-19 15:07:49 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2274,7 +2274,7 @@ const modalOpen = ref(false);
       </div>
 
       <!-- inbox messages div -->
-      <div v-if="modalOpen" class="fixed inset-0 bg-black opacity-50 z-40">
+      <div v-if="modalOpen" class="fixed inset-0 bg-black opacity-50 z-40"></>
       <div v-show="showinbox" class="h-screen flex flex-col justify-between">
         <div class="h-full flex flex-col overflow-y-hidden">
           <div class="flex flex-col">
@@ -2331,7 +2331,6 @@ const modalOpen = ref(false);
           <p>inbox messages</p>
         </div>
       </div>
-     </div>
 
       <!-- messages conversation div -->
       <div v-show="showmessenger" class="flex flex-col justify-between h-screen" ref="messengeContainler">
